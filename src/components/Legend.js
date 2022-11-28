2bc9dcdbfd2fb557731f3ff5157fc005a0340d52
nemanjadabi <dbzlj00@gmail.com>
2022-11-28 18:44:55 +0100

    css for Legend component

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -1,18 +1,35 @@
-import './Legend.css'
+import "./Legend.css";
+const markerColor = {
+  Excellent: "#47dfaa",
+  Good: "#29b4e3",
+  Accaptable: "#f2f26d",
+  Polluted: "#8c3998",
+  "Very Polluted": "#db0456",
+};
+
 const Legend = () => {
-    return (
-        
-<ul className="legend">
-    
-    <li className='item_one'><span>Excellent</span></li>
-    <li className='item_two'><span>Good</span></li>
-    <li className='item_three'><span>Acceptable</span></li>
-    <li className='item_four'><span>Polluted</span></li>
-    <li className='item_five'><span>VeryPolluted</span></li>
+  return (
+    <ul className="legend">
+      <li className="item" style={{ backgroundColor: markerColor.Excellent }}>
+        <span>Excellent</span>
+      </li>
+      <li className="item" style={{ backgroundColor: markerColor.Good }}>
+        <span>Good</span>
+      </li>
+      <li className="item" style={{ backgroundColor: markerColor.Accaptable }}>
+        <span>Acceptable</span>
+      </li>
+      <li className="item" style={{ backgroundColor: markerColor.Polluted }}>
+        <span>Polluted</span>
+      </li>
+      <li
+        className="item"
+        style={{ backgroundColor: markerColor["Very Polluted"] }}
+      >
+        <span>Very Polluted</span>
+      </li>
+    </ul>
+  );
+};
 
-</ul>
-
-    )
-}
-
-export default Legend
+export default Legend;
